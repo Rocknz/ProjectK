04f1a636b7b83587484d46210e2a60e64c733a3d
Rocknz <JungRockJoon@jeong-ui-MacBook-Pro.local>
2013-09-14 23:09:56 +0900

    ProjectK

---
+++ Assets/Breaking.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breaking : MonoBehaviour{
+	static int sIZE = 8;
+	public bool Notice = true; // is breaking box ?
+	public int[] map = new int[sIZE*sIZE+1];
+	public int[] Breakmap = new int[sIZE*sIZE+1];
+	public GameObject[] list = new GameObject[sIZE*sIZE+1];
+	tk2dSpriteCollectionData datas;
+	
+	public void input(int[] m,GameObject[] l,tk2dSpriteCollectionData data){
+		this.map = m;	
+		this.list = l;
+		this.datas = data;
+	}
+	public IEnumerator breaking(){
+		
+		Notice = false;
+		Breakis();
+		Breakblock();
+		
+		if(Notice){
+			yield return StartCoroutine("BreakingAni"); // animation;
+			yield return StartCoroutine("DownAni");
+		}
+		yield return null;
+	}
+	public void go(int x,int y,int px,int py){
+		int cnt = 0;
+		int xx,yy;
+		int now;
+		xx = x;
+		yy = y;
+		now = map[x*sIZE+y];
+		while(map[xx*sIZE+yy] == now){
+			cnt ++;
+			xx += px;
+			yy += py;
+			if( xx < 0 || xx >= sIZE || yy < 0 || yy >=sIZE)
+				break;
+		}
+		if(cnt >= 3){
+			xx = x;
+			yy = y;
+			while(map[xx*sIZE+yy] == now){
+				Breakmap[xx*sIZE+yy] = 1;
+				xx += px;
+				yy += py;
+				if( xx < 0 || xx >= sIZE || yy < 0 || yy >=sIZE)
+					break;
+			}
+		}
+	}
+	public void Breakis(){
+		int i,j;
+		for(i=0;i<sIZE;i++){
+			for(j=0;j<sIZE;j++){
+				Breakmap[i*sIZE+j] = 0;
+			}
+		}
+		for(i=0;i<sIZE;i++){
+			for(j=0;j<sIZE;j++){
+				go(i,j,0,1);
+				go(i,j,0,-1);
+				go(i,j,1,0);
+				go(i,j,-1,0);
+			}
+		}
+	}
+	public void Breakblock(){
+		int i;
+		for(i=0;i<sIZE*sIZE;i++){
+			if(Breakmap[i] == 1){
+				map[i] = -1;
+				Notice = true;
+			}
+		}
+	}
+	public IEnumerator DownAni(){
+		int i;
+		bool check = true;
+		while(check){
+			check = false;
+			for(i=0;i<sIZE*sIZE;i++){
+				if(map[i] == -1){
+					check = true;
+					if(i >= (sIZE*sIZE-sIZE)){
+						int t = (int)(Random.value * 6.0f);
+						if(t == 6){
+							map[i] = t-1;
+						}else{
+							map[i] = t;
+						}
+					}
+					else{
+						map[i] = map[i+sIZE];
+						map[i+sIZE] = -1;
+					}
+				}
+			}
+			if(check){
+				yield return StartCoroutine("showMap");
+			}
+		}
+	}
+	public IEnumerator showMap(){
+		for(int i=0;i<sIZE*sIZE;i++){
+			SetGO (list[i]);	
+		}
+		yield return new WaitForSeconds(1.0f);
+	}
+	public IEnumerator BreakingAni(){
+		
+		yield return StartCoroutine("showMap");
+	}
+	public void SetGO(GameObject go){
+		int a = int.Parse (go.name);
+		tk2dSprite sprite = go.GetComponent<tk2dSprite>();
+		switch(map[a]){
+			case -1 : sprite.SetSprite(datas,"0"); break;                            //breaked;
+			case 0: sprite.SetSprite(datas,"1"); break;
+			case 1: sprite.SetSprite(datas,"2"); break;
+			case 2: sprite.SetSprite(datas,"3"); break;
+			case 3: sprite.SetSprite(datas,"4"); break;
+			case 4: sprite.SetSprite(datas,"5"); break;
+			case 5: sprite.SetSprite(datas,"6"); break;
+		}
+	}
+}
---
+++ Assets/Making.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Making : MonoBehaviour {
+	static int sIZE = 8;
+	// Use this for initializat ion
+	GameObject ob;
+	GameObject ob2;
+	GameObject[] list = new GameObject[sIZE*sIZE+1];
+	int[] map = new int[sIZE*sIZE+1];
+	tk2dSpriteCollectionData datas;
+	void Start () {
+		gameObject.AddComponent<Breaking>();
+		/*
+		ob = new GameObject("Rock");
+		tk2dSpriteCollectionData data = (tk2dSpriteCollectionData)Resources.Load("spriteC/SpriteC",typeof(tk2dSpriteCollectionData));
+		tk2dSprite sp = ob.AddComponent<tk2dSprite>();
+		sp.SetSprite(data,"ground");
+		ob.transform.localScale = (new Vector3(10.0f,10.0f,10.0f));
+		ob.transform.localPosition = (new Vector3(10.0f,10.0f,10.0f));
+		*/
+		datas = (tk2dSpriteCollectionData)Resources.Load("Box/BoxCollections",typeof(tk2dSpriteCollectionData));
+		for(int i=0;i<sIZE;i++){
+			for(int j=0;j<sIZE;j++){
+				int gap = i*sIZE+j;
+				list[gap] = new GameObject(gap.ToString());
+				// Save Index(x,y) at Tag
+				
+				list[gap].AddComponent<tk2dSprite>();
+				int t = (int)(Random.value * 6.0f);
+				if(t == 6){
+					map[gap] = t-1;
+				}else{
+					map[gap] = t;
+				}
+				SetGO (list[gap]);
+				list[gap].transform.localScale = (new Vector3(1.0f,1.0f,1.0f));
+				list[gap].transform.localPosition = (new Vector3((float)(j*10.0f),(float)(i*10.0f),0.1f));
+				list[gap].AddComponent<BoxCollider>();
+			}
+		}
+		StartCoroutine("BreakBlock");
+	}
+	
+	// Update is called once per framevoid Update()
+	// state of Listenable;
+	bool state = false;
+	GameObject target;
+	
+	void Update()
+	{
+		if(state){
+			StartCoroutine("Listener");	
+		}
+	}
+	
+	bool iSnear(int f,int s){
+		if(Mathf.Abs (f-s) == sIZE){
+			return true;
+		}
+		else if(Mathf.Abs (f-s) == 1){
+			if((f % sIZE == 0 && s % sIZE == sIZE-1) ||
+				(s % sIZE == 0 && f % sIZE == sIZE-1)){
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	public void SetGO(GameObject go){
+		int a = int.Parse (go.name);
+		tk2dSprite sprite = go.GetComponent<tk2dSprite>();
+		switch(map[a]){
+			case -1 : sprite.SetSprite(datas,"0"); break;                            //breaked;
+			case 0: sprite.SetSprite(datas,"1"); break;
+			case 1: sprite.SetSprite(datas,"2"); break;
+			case 2: sprite.SetSprite(datas,"3"); break;
+			case 3: sprite.SetSprite(datas,"4"); break;
+			case 4: sprite.SetSprite(datas,"5"); break;
+			case 5: sprite.SetSprite(datas,"6"); break;
+		}
+	}
+	
+	public GameObject GetTarget()
+	{
+        RaycastHit hit;
+        GameObject target = null; 
+    	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+		Vector3 start = ray.origin;
+		Vector3 direction = ray.direction*(1.0f);
+        if( true == (Physics.Raycast(start, direction, out hit))) 
+        {
+            target = hit.collider.gameObject; 
+        } 
+        return target; 
+	}
+	
+	
+	public IEnumerator Listener(){
+		state = false;
+		if(Input.GetMouseButtonDown(0)){
+			
+		}
+		if(Input.GetMouseButtonUp(0)){
+			if(target == null){
+				target = GetTarget();
+				if(target != null){
+					state = true;
+					target.transform.localScale = (new Vector3(1.2f,1.2f,1.2f));
+				}
+			}
+			else{
+				GameObject target2;
+				target2 = GetTarget();
+				if(target2 != null){
+					int f,s,temp;
+					f = int.Parse(target.name);
+					s = int.Parse(target2.name);
+					if(iSnear(f,s)){
+						temp = map[f];
+						map[f] = map[s];
+						map[s] = temp;
+						
+						SetGO (target);
+						SetGO (target2);
+					}
+					else{
+						// notification
+						
+					}
+					target.transform.localScale = (new Vector3(1.0f,1.0f,1.0f));
+					target = null;
+					yield return StartCoroutine("BreakBlock");
+				}
+			}
+		}
+		
+		
+		/*
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+			ob2 = GameObject.Find("Rock");
+			ob2.transform.localPosition = (new Vector3(20.0f,20.0f,20.0f));
+	    }
+		//Mouse Move Event
+		
+		if(state){
+			Vector3 MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+            target.transform.position = new Vector3(MousePos.x, MousePos.y, target.transform.position.z);
+       	}*/
+		
+		state = true;
+		yield return null;
+	}
+	IEnumerator BreakBlock(){ //breaking all combo 
+		//breaking is only breaking one time;
+		Breaking b = gameObject.GetComponent<Breaking>();
+		b.input(map,list,datas);
+		b.Notice = true;
+		while(b.Notice){
+			yield return b.StartCoroutine("breaking");
+		}
+		state = true; // it will be trigger to work listener 
+		map = b.map;
+		yield return null;
+	}
+}
